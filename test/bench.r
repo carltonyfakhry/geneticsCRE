281620517e74be410e2fc272625a5866327e4791
Dmitri Bichko <dbichko@gmail.com>
2017-04-09 00:26:48 -0400

    m

--- test/bench.r
+++ test/bench.r
@@ -1,5 +1,7 @@
 library(devtools)
-load_all()
+
+load_all(recompile=TRUE)
+#load_all()
 
 # res2 <- GetBestPaths('/data/gcre/testdata', nCases = 1537, nControls = 1537, method = 'method2-old', threshold_percent = 0.05, K = 2, pathLength = 5, iterations = 4, strataF = NA, nthreads = 2)
 # res2 <- GetBestPaths('/data/gcre/testdata', nCases = 1537, nControls = 1537, method = 'method2', threshold_percent = 0.05, K = 8, pathLength = 3, iterations = 100, strataF = NA, nthreads = 1)
