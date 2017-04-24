db3dad9e0b7b780262f4985704bccd07cc79b986
Dmitri Bichko <dbichko@gmail.com>
2017-04-23 23:35:05 -0400

    debug scripts

--- test/issue-020.r
+++ test/issue-020.r
@@ -3,13 +3,15 @@ suppressMessages(library(plyr))
 suppressMessages(library(dplyr))
 load_all()
 
-data <- '/data/gcre/testdata'
+# data <- '/data/gcre/issue-020/testdata'
+data <- '/data/gcre/testdata2'
 len <- 1
-m <- 2
+m <- 1
+cases <- 1000
 
-res2 <- GetBestPaths(dataset = data, nCases = 1537, nControls = 1537, method = m,
+res2 <- GetBestPaths(dataset = data, nCases = cases, nControls = cases, method = m,
 threshold_percent = 1, K = 1000, pathLength = len, iterations = 10, strataF = NA, nthreads = 4)
 
-checkBestPaths(dataset = data, BestPaths = res2, pathLength = len, nCases = 1537, nControls = 1537, method = m)
+checkBestPaths(dataset = data, BestPaths = res2, pathLength = len, nCases = cases, nControls = cases, method = m)
 
 # res2[,c("Paths", "Scores", "Pvalues")]
