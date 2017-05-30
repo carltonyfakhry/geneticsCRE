972a87082d3bd6d6f276948d1d4584c8f766f205
kolonel <carlfakhry@gmail.com>
2017-05-30 18:10:24 -0400

    Fixed score_permute function for method 2 so it computes the new version of the score

--- src/methods.h
+++ src/methods.h
@@ -155,7 +155,8 @@ public:
     uint32_t total_pos = 0;
     uint32_t total_neg = 0;
 
-    uint64_t bit_pos, bit_neg, bit_con, true_pos, true_neg, mask;
+    // uint64_t bit_pos, bit_neg, bit_con, true_pos, true_neg, mask;
+    uint64_t bit_pos, bit_neg, mask;
 
     for(int k = 0; k < width_ul; k++) {
 
@@ -164,29 +165,38 @@ public:
 
       if(bit_pos != 0 || bit_neg != 0) {
 
-        bit_con = bit_pos & bit_neg;
-        true_pos = bit_pos & ~bit_con;
-        true_neg = bit_neg & ~bit_con;
+        // bit_con = bit_pos & bit_neg;
+        // true_pos = bit_pos & ~bit_con;
+        // true_neg = bit_neg & ~bit_con;
         mask = case_mask[k];
 
-        total_pos += __builtin_popcountl(true_pos);
-        total_neg += __builtin_popcountl(true_neg);
-        case_pos += __builtin_popcountl(true_pos &  mask);
-        case_neg += __builtin_popcountl(true_neg & ~mask);
-        ctrl_pos += __builtin_popcountl(true_neg &  mask);
-        ctrl_neg += __builtin_popcountl(true_pos & ~mask);
+        // total_pos += __builtin_popcountl(true_pos);
+        // total_neg += __builtin_popcountl(true_neg);
+        // case_pos += __builtin_popcountl(true_pos &  mask);
+        // case_neg += __builtin_popcountl(true_neg & ~mask);
+        // ctrl_pos += __builtin_popcountl(true_neg &  mask);
+        // ctrl_neg += __builtin_popcountl(true_pos & ~mask);
+        total_pos += __builtin_popcountl(bit_pos);
+        total_neg += __builtin_popcountl(bit_neg);
+        case_pos += __builtin_popcountl(bit_pos &  mask);
+        case_neg += __builtin_popcountl(bit_neg & ~mask);
+        ctrl_pos += __builtin_popcountl(bit_neg &  mask);
+        ctrl_neg += __builtin_popcountl(bit_pos & ~mask);
 
-        if(true_pos != 0) {
+        // if(true_pos != 0) {
+        if(bit_pos != 0) {
           const uint64_t* p_mask = perm_case_mask + k * iters;
           for(int r = 0; r < iters; r++) {
-            perm_case_pos[r] += __builtin_popcountl(true_pos & p_mask[r]);
+            // perm_case_pos[r] += __builtin_popcountl(true_pos & p_mask[r]);
+            perm_case_pos[r] += __builtin_popcountl(bit_pos & p_mask[r]);
           }
         }
 
-        if(true_neg != 0) {
+        // if(true_neg != 0) {
           const uint64_t* p_mask = perm_case_mask + k * iters;
           for(int r = 0; r < iters; r++){
-            perm_ctrl_pos[r] += __builtin_popcountl(true_neg & p_mask[r]);
+            // perm_ctrl_pos[r] += __builtin_popcountl(true_neg & p_mask[r]);
+            perm_ctrl_pos[r] += __builtin_popcountl(bit_neg & p_mask[r]);
           }
         }
 
@@ -199,16 +209,20 @@ public:
 
     }
 
-    int cases = case_pos + case_neg;
-    int ctrls = ctrl_pos + ctrl_neg;
+    // int cases = case_pos + case_neg;
+    // int ctrls = ctrl_pos + ctrl_neg;
 
-    keep_score(idx, loc, cases, ctrls);
+    // keep_score(idx, loc, cases, ctrls);
+    keep_score(idx, loc, case_pos, ctrl_neg, case_neg, ctrl_pos);
 
     for(int r = 0; r < iters; r++){
-      int perm_cases = perm_case_pos[r] + (total_neg - perm_ctrl_pos[r]);
-      int perm_ctrls = perm_ctrl_pos[r] + (total_pos - perm_case_pos[r]);
+      // int perm_cases = perm_case_pos[r] + (total_neg - perm_ctrl_pos[r]);
+      // int perm_ctrls = perm_ctrl_pos[r] + (total_pos - perm_case_pos[r]);
+      int perm_case_neg = total_neg - perm_ctrl_pos[r];
+      int perm_ctrl_neg = total_pos - perm_case_pos[r];
 
-      double p_score = value_table_max[perm_cases][perm_ctrls];
+      // double p_score = value_table_max[perm_cases][perm_ctrls];
+      double p_score = value_table_max[perm_case_pos[r]][perm_ctrl_neg] + value_table_max[perm_case_neg][perm_ctrl_pos[r]];
       if(p_score > perm_scores[r])
         perm_scores[r] = p_score;
     }
@@ -225,10 +239,23 @@ protected:
     double score = value_table[cases][ctrls];
     if(score > scores.top().score)
       scores.push(Score(score, idx, loc, cases, ctrls));
-    
-    double flips = value_table[ctrls][cases];
-    if(flips > scores.top().score)
-      scores.push(Score(flips, idx, loc + flip_pivot_len, cases, ctrls));
+
+    // double flips = value_table[ctrls][cases];
+    // if(flips > scores.top().score)
+    //   scores.push(Score(flips, idx, loc + flip_pivot_len, cases, ctrls));
+    while(scores.size() > exec->top_k)
+      scores.pop();
+
+  }
+
+  void keep_score(int idx, int loc, int case_pos, int ctrl_neg, int case_neg, int ctrl_pos) {
+
+    double score = value_table[case_pos][ctrl_neg] + value_table[case_neg][ctrl_pos];
+    int cases = case_pos + case_neg;
+    int ctrls = ctrl_pos + ctrl_neg;
+    if(score > scores.top().score)
+      scores.push(Score(score, idx, loc, cases, ctrls));
+
     while(scores.size() > exec->top_k)
       scores.pop();
 
